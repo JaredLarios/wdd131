5bfa85170718286f4be04eb8dfbed97e9cc8c19d
jedlarios <lar21038@byui.edu>
2024-04-25 17:57:47 -0600

    feat: delete element from list added and whipe data and refocus to input

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,31 +1,38 @@
 // Get Web Element
-const input = document.querySelector('#favchap')
-const button = document.querySelector('button')
-const list = document.querySelector('#list')
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
 // Craete Web Elements
-const li = document.createElement('li')
-const deleteButton = document.createElement('button')
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
 
 // Append Element in list
 const postElement = () => {
     // If not vale on input
     if (input.value.trim() !== '') {
         // Populate Web Elements
-        li.textContent = input.value
-        deleteButton.textContent = 'x'
+        li.textContent = input.value;
+        deleteButton.textContent = 'x';
 
         // Render DOM Element
-        li.append(deleteButton)
-        list.append(li)
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+        input.focus();
     } else {
         // Focus input element if is empty
-        input.focus()
+        input.focus();
     }
 }
 
-// TODO: add deleted action 
+const deleteElement = () => {
+    // Remove elements
+    list.removeChild(li);
+    input.focus();
+}
 
 // DOM Event
-button.addEventListener('click', postElement)
-// TODO: add event listener to remove element
+button.addEventListener('click', postElement);
+deleteButton.addEventListener('click', deleteElement);
