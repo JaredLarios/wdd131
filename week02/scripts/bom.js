d8e224592aa2c820a38441bafc146b51180973ab
jedlarios <lar21038@byui.edu>
2024-04-25 19:30:26 -0600

    feat: post, validate input and render to DOM the top 10 books and chapters

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,23 +1,36 @@
 // Get Web Element
+const LIMIT = 10
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-// Craete Web Elements
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
 // Append Element in list
 const postElement = () => {
+    let listLi = document.querySelectorAll('li').length
     // If not vale on input
     if (input.value.trim() !== '') {
+        // Craete Web Elements
+        let li = document.createElement('li');
+        let deleteButton = document.createElement('button');
+
         // Populate Web Elements
         li.textContent = input.value;
         deleteButton.textContent = 'x';
 
-        // Render DOM Element
-        li.append(deleteButton);
-        list.append(li);
+        if (listLi >= LIMIT) {
+            alert('You exceed the 10 books and chapters on this app.')
+        } else {
+            // Render DOM Element
+            li.append(deleteButton);
+            list.append(li);
+        }
+        
+        deleteButton.addEventListener('click', () => {
+            // Remove elements
+            list.removeChild(li);
+            input.focus();
+        })
 
         input.value = '';
         input.focus();
@@ -27,12 +40,5 @@ const postElement = () => {
     }
 }
 
-const deleteElement = () => {
-    // Remove elements
-    list.removeChild(li);
-    input.focus();
-}
-
 // DOM Event
 button.addEventListener('click', postElement);
-deleteButton.addEventListener('click', deleteElement);
