1bb075599b71696b18b083ab2a433b0555cf7f10
jedlarios <lar21038@byui.edu>
2024-04-28 00:04:39 -0600

    style: small view

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,8 @@
-// get DOM Elements
-const currentYear = document.querySelector('#year')
-const lastModification = document.querySelector('#lastModification')
+// Get DOM Elements
+const currentYear = document.querySelector('#year');
+const lastModification = document.querySelector('#lastModification');
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
 
 // Get Dates
 const today = new Date()
@@ -9,3 +11,9 @@ const lastModificationDate = new Date(document.lastModified)
 // Render to DOM Content
 currentYear.innerHTML = `${today.getFullYear()}`
 lastModification.innerHTML = `Last Modification: ${lastModificationDate}`
+
+// Eevents Listener
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+})
