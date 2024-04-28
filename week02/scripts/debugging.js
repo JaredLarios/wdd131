932785d53605db9a765c380fab6ceaf20b671d42
jedlarios <lar21038@byui.edu>
2024-04-27 19:55:22 -0600

    feat: debugger js activity 2

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -1,3 +1,4 @@
+// Activity 1
 const PI = 3.14;
 let radius = 3;
 let area = 0;
@@ -5,4 +6,16 @@ area = radius * radius * PI;
 console.log('Area1: ' + area)
 radius = 4;
 area = radius * radius * PI;
-console.log("Area2: " + area)
+console.log('Area2: ' + area)
+
+// Activity 2
+function circleArea(radius) {
+    // return area
+    return (radius**2) * PI;
+}
+
+area = circleArea(3)
+console.log('Area3: ' + area)
+
+area = circleArea(4)
+console.log('Area4: ' + area)
