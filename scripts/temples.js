9ec60ac0b59c8abdd363b116fb53a65cf5d4d7ae
jedlarios <lar21038@byui.edu>
2024-04-27 20:43:14 -0600

    chore: comments added

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,11 @@
+// get DOM Elements
 const currentYear = document.querySelector('#year')
 const lastModification = document.querySelector('#lastModification')
 
+// Get Dates
 const today = new Date()
 const lastModificationDate = new Date(document.lastModified)
 
+// Render to DOM Content
 currentYear.innerHTML = `${today.getFullYear()}`
 lastModification.innerHTML = `Last Modification: ${lastModificationDate}`
