5b26aa597e9ccdaab39e60670509487170e9bb7a
JaredLarios <lar21038@byui.edu>
2024-05-29 20:40:15 -0600

    style: services layout and placeholders made

---
+++ project/scripts/service.js
@@ -0,0 +1,58 @@
+const grid = document.querySelector('.grid');
+
+const listServices = [
+    {
+        title: "Software Developement",
+        content: "",
+        img: "./images/team-small.webp",
+    },
+    {
+        title: "Web Developement",
+        content: "",
+        img: "./images/team-small.webp",
+    },
+    {
+        title: "UIX Design",
+        content: "",
+        img: "./images/team-small.webp",
+    },
+    {
+        title: "Front-End",
+        content: "",
+        img: "./images/team-small.webp",
+    },
+    {
+        title: "Back-End",
+        content: "",
+        img: "./images/team-small.webp",
+    },
+    {
+        title: "API",
+        content: "",
+        img: "./images/team-small.webp",
+    }
+];
+
+displayCard(listServices);
+
+function displayCard(itemList) {
+    grid.innerHTML = ``;
+    itemList.forEach(item => {
+        let card = document.createElement('div');
+        let title = document.createElement('h3');
+        let content = document.createElement('p');
+        let img = document.createElement('img');
+
+        title.textContent = item.title;
+        content.textContent = item.content;
+        img.setAttribute("src", item.img);
+        img.setAttribute("alt", item.title);
+        img.setAttribute("loading", "lazy");
+
+        card.appendChild(title);
+        card.appendChild(content);
+        card.appendChild(img);
+
+        grid.appendChild(card);
+    });
+}
